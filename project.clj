a78d6de1256386d5a91214ef0999513496041849
Marco Dalla Stella <marco@7bridges.eu>
2017-07-08 22:40:44 +0200

    Added cloverage for testing coverage.

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [midje "1.8.3"]
                  [midje-junit-formatter "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-midje "3.1.3"]])
+  :plugins [[lein-midje "3.1.3"]
+            [lein-cloverage "1.0.9"]]
+  :aliases {"coverage" ["cloverage" "--runner" ":midje"]})
