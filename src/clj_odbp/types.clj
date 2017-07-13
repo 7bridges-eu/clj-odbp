c46b5291a9a14d4324d77ea177c595e16d8d1d02
Manuel Uberti <manuel@7bridges.org>
2017-07-13 12:52:43 +0200

    Add custom OrientDB types

--- src/clj_odbp/types.clj
+++ src/clj_odbp/types.clj
@@ -1,1 +1,34 @@
-(ns clj-odbp.types)
+(ns clj-odbp.types
+  (:import
+   [java.text DateFormat SimpleDateFormat]
+   [java.util Date]))
+
+(defprotocol OrientType
+  (serialize [this]))
+
+(deftype OrientBase64 [value]
+  OrientType
+  (serialize [this] value))
+
+(defn orient-base64 [value]
+  (->OrientBase64 value))
+
+(deftype OrientDate [value]
+  OrientType
+  (serialize [this]
+    (let [formatter (SimpleDateFormat. "dd/MM/yyyy")
+          date-without-time (.parse formatter (.format formatter value))
+          date->long (.getTime date-without-time)]
+      (str date->long "a"))))
+
+(defn orient-date [value]
+  (->OrientDate value))
+
+(deftype OrientDateTime [value]
+  OrientType
+  (serialize [this]
+    (let [date->long (.getTime value)]
+      (str date->long "t"))))
+
+(defn orient-date-time [value]
+  (->OrientDateTime value))
