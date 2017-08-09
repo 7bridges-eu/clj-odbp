23766110b9701ad6b3aac3f0d1eccf1921904259
Marco Dalla Stella <marco@7bridges.eu>
2017-08-09 10:49:04 +0200

    Removed useless println from clj-odbp.deserialize.binary.otypes.

--- src/clj_odbp/deserialize/binary/otypes.clj
+++ src/clj_odbp/deserialize/binary/otypes.clj
@@ -157,6 +157,7 @@
   (let [version (read-version buffer)
         class-name (read-class-name buffer)
         headers (read-headers buffer)]
+    (println headers)
     {class-name (read-record headers buffer)}))
 
 (defmethod deserialize :embedded-list-orient-type
