a42150bc01260fdfce61799d5c24287fc9ed9d28
Manuel Uberti <manuel@7bridges.eu>
2017-08-22 17:27:19 +0200

    Add serialization of custom types

--- src/clj_odbp/serialize/binary/otypes.clj
+++ src/clj_odbp/serialize/binary/otypes.clj
@@ -1,8 +1,19 @@
 (ns clj-odbp.serialize.binary.otypes
-  (:require [clj-odbp.serialize.binary
+  (:require [clj-odbp.constants :as const]
+            [clj-odbp.serialize.binary
              [common :as c]
              [int :as i]
              [varint :as v]]))
+
+(def orient-types
+  {:boolean-type (byte 0) :integer-type (byte 1) :short-type (byte 2)
+   :long-type (byte 3) :float-type (byte 4) :double-type (byte 5)
+   :datetime-type (byte 6) :string-type (byte 7) :keyword-type (byte 7)
+   :binary-type (byte 8) :embedded-record-type (byte 9)
+   :embedded-list-type (byte 10) :embedded-set-type (byte 11)
+   :embedded-map-type (byte 12) :link-type (byte 13) :link-list-type (byte 14)
+   :link-set-type (byte 15) :link-map-type (byte 16) :byte-type (byte 17)
+   :custom-type (byte 20) :decimal-type (byte 21) :any-type (byte 23)})
 
 (defn link?
   [v]
@@ -39,75 +50,317 @@
     (instance? Long v) :integer-type
     (instance? Float v) :float-type
     (instance? Double v) :double-type
-    (instance? java.util.Date v) :datetime-type
-    ;; (instance? OrientBinary v) :binary-type
-    (embedded-record? v) :embedded-record
     (instance? Byte v) :byte-type
     (instance? java.math.BigDecimal v) :decimal-type
+    (instance? java.util.Date v) :datetime-type
+    (keyword? v) :keyword-type
+    ;; (instance? OrientBinary v) :binary-type
     (link? v) :link-type
     (string? v) :string-type
-    (keyword? v) :keyword-type
     (link-list? v) :link-list-type
     (link-set? v) :link-set-type
     (link-map? v) :link-map-type
+    (embedded-record? v) :embedded-record-type
     (sequential? v) :embedded-list-type
     (set? v) :embedded-set-type
     (map? v) :embedded-map-type
-    :else :any-type))
+    :else :custom-type))
 
-(defmulti serialize get-type)
+(defmulti serialize (fn [value & offset] (get-type value)))
 
 (defmethod serialize :boolean-type
-  [value]
-  (if value
-    [(byte 1)]
-    [(byte 0)]))
+  ([value]
+   (if value
+     [(byte 1)]
+     [(byte 0)]))
+  ([value offset]
+   (serialize value)))
 
 (defmethod serialize :integer-type
-  [value]
-  (v/varint-unsigned value))
+  ([value]
+   (v/varint-unsigned value))
+  ([value offset]
+   (serialize value)))
 
 (defmethod serialize :float-type
-  [value]
-  (-> (java.nio.ByteBuffer/allocate 4)
-      (.putFloat value)
-      .array
-      vec))
+  ([value]
+   (-> (java.nio.ByteBuffer/allocate 4)
+       (.putFloat value)
+       .array
+       vec))
+  ([value offset]
+   (serialize value)))
 
 (defmethod serialize :double-type
-  [value]
-  (-> (java.nio.ByteBuffer/allocate 8)
-      (.putDouble value)
-      .array
-      vec))
-
-(defmethod serialize :datetime-type
-  [value]
-  (serialize (.getTime value)))
-
-(defmethod serialize :decimal-type
-  [value]
-  (let [scale (i/int32 (.scale value))
-        serialized-value (-> value
-                             .unscaledValue
-                             .toByteArray
-                             vec)
-        value-size (i/int32 (count serialized-value))]
-    (vec (concat scale value-size serialized-value))))
-
-(defmethod serialize :any-type
-  [value]
-  value)
-
-(defmethod serialize :string-type
-  [value]
-  (let [bytes (.getBytes value "UTF-8")]
-    (c/bytes-type bytes)))
-
-(defmethod serialize :keyword-type
-  [value]
-  (serialize (name value)))
+  ([value]
+   (-> (java.nio.ByteBuffer/allocate 8)
+       (.putDouble value)
+       .array
+       vec))
+  ([value offset]
+   (serialize value)))
 
 (defmethod serialize :byte-type
+  ([value]
+   [value])
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :decimal-type
+  ([value]
+   (let [scale (i/int32 (.scale value))
+         serialized-value (-> value
+                              .unscaledValue
+                              .toByteArray
+                              vec)
+         value-size (i/int32 (count serialized-value))]
+     (vec (concat scale value-size serialized-value))))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :datetime-type
+  ([value]
+   (serialize (.getTime value)))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :string-type
+  ([value]
+   (let [bytes (.getBytes value "UTF-8")]
+     (c/bytes-type bytes)))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :keyword-type
+  ([value]
+   (serialize (name value)))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :link-type
+  ([value]
+   (let [rid (clojure.string/split (subs value 1) #":")
+         cluster-id (Integer/parseInt (first rid))
+         record-position (Integer/parseInt (second rid))
+         cid-varint (v/varint-unsigned cluster-id)
+         rpos-varint (v/varint-unsigned record-position)]
+     (vec (concat cid-varint rpos-varint))))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :link-list-type
+  ([value]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         serialized-items (mapcat serialize value)]
+     (vec (concat size-varint serialized-items))))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :link-set-type
+  ([value]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         serialized-items (mapcat serialize value)]
+     (vec (concat size-varint serialized-items))))
+  ([value offset]
+   (serialize value)))
+
+(defn serialize-key-value
+  [k v]
+  (let [key-type [(get orient-types (get-type k))]
+        key-value (serialize k)
+        link (serialize v)]
+    (vec (concat key-type key-value link))))
+
+(defmethod serialize :link-map-type
+  ([value]
+   (let [size (v/varint-unsigned (count value))
+         key-values (mapcat (fn [[k v]] (serialize-key-value k v)) value)]
+     (vec (concat size key-values))))
+  ([value offset]
+   (serialize value)))
+
+(defn serialize-list-item
   [value]
-  [value])
+  (let [t [(get orient-types (get-type value))]
+        v (serialize value)]
+    (into t v)))
+
+(defmethod serialize :embedded-list-type
+  ([value]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         serialized-items (vec (apply concat (map serialize-list-item value)))
+         any [(byte 23)]]
+     (vec (concat size-varint any serialized-items))))
+  ([value offset]
+   (serialize value)))
+
+(defmethod serialize :embedded-set-type
+  ([value]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         serialized-items (vec (apply concat (map serialize-list-item value)))
+         any [(byte 23)]]
+     (vec (concat size-varint any serialized-items))))
+  ([value offset]
+   (serialize value)))
+
+(defn get-structure
+  [record-map]
+  (reduce
+   (fn [acc k]
+     (let [record-map-value (get record-map k)]
+       (conj acc {:key-type (get orient-types (get-type k))
+                  :field-name k
+                  :position 0
+                  :type (get orient-types (get-type record-map-value))
+                  :value record-map-value})))
+   []
+   (keys record-map)))
+
+(defn header-size
+  [headers fixed-header-int]
+  (+ 1                                  ; closing header
+     (reduce
+      (fn [acc k]
+        (+ acc (count (serialize k)) fixed-header-int))
+      0
+      headers)))
+
+(defn serialize-structure-values
+  [structure]
+  (map
+   (fn [s]
+     (let [v (:value s)]
+       (assoc s :serialized-value (serialize v))))
+   structure))
+
+(defn oemap-positions
+  [structure offset]
+  (let [hsize (header-size
+               (map :field-name structure) const/fixed-oemap-header-int)]
+    (reduce
+     (fn [acc s]
+       (if (empty? acc)
+         (conj acc
+               (assoc s :position (+ offset hsize)))
+         (conj acc
+               (assoc s :position
+                      (+ (count (:serialized-value (last acc)))
+                         (:position (last acc)))))))
+     []
+     structure)))
+
+(defn orient-int32
+  [value]
+  (i/int32 (int value)))
+
+(defn positions->orient-int32
+  [structure]
+  (map #(update % :position orient-int32) structure))
+
+(defn oemap->structure
+  [data-map offset]
+  (-> (get-structure data-map)
+      serialize-structure-values
+      (oemap-positions offset)
+      positions->orient-int32))
+
+(defn serialize-elements
+  [header key-order]
+  (reduce
+   (fn [acc hk]
+     (if (= hk :position)
+       (conj acc (get header hk))
+       (conj acc (serialize (get header hk)))))
+   []
+   key-order))
+
+(defn serialize-headers
+  [structure key-order]
+  (mapcat
+   #(serialize-elements % key-order)
+   structure))
+
+(defn serialize-data
+  [structure]
+  (->> structure
+       (map :serialized-value)))
+
+(defmethod serialize :embedded-map-type
+  ([value]
+   (serialize value 0))
+  ([value offset]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         structure (oemap->structure value offset)
+         key-order [:key-type :field-name :position :type]
+         serialized-headers (serialize-headers structure key-order)
+         serialized-data (serialize-data structure)]
+     (-> (concat size-varint serialized-headers serialized-data)
+         flatten
+         vec))))
+
+(defn first-elem
+  [record-map offset]
+  (let [f (first record-map)
+        k (first f)
+        v (second f)
+        hsize (header-size (keys record-map) const/fixed-header-int)]
+    {:key-type (get orient-types (get-type k))
+     :field-name k
+     :type (get orient-types (get-type v))
+     :value v
+     :serialized-value (serialize v (+ 1 offset hsize))
+     :position (+ 1 offset hsize)}))
+
+(defn rest-elem
+  [record-map first-elem]
+  (reduce
+   (fn [acc [k v]]
+     (let [last-elem (last acc)
+           serialized-elem (:serialized-value last-elem)
+           size-le (count serialized-elem)
+           pos (+ size-le (:position last-elem))]
+       (conj
+        acc
+        {:key-type (get orient-types (get-type k))
+         :field-name k
+         :type (get orient-types (get-type v))
+         :value v
+         :position pos
+         :serialized-value (serialize v pos)})))
+   (conj [] first-elem)
+   (rest record-map)))
+
+(defn record-map->structure
+  [record-map offset]
+  (->> (first-elem record-map offset)
+       (rest-elem record-map)
+       positions->orient-int32))
+
+(defmethod serialize :embedded-record-type
+  ([value]
+   (serialize value 0))
+  ([value offset]
+   (let [size (count value)
+         size-varint (v/varint-unsigned size)
+         class (get value :_class "")
+         serialized-class (serialize class)
+         serialized-class-size (count serialized-class)
+         first-elem-pos (dec (+ offset serialized-class-size))
+         structure (record-map->structure value first-elem-pos)
+         key-order [:field-name :position :type]
+         serialized-headers (serialize-headers structure key-order)
+         end-headers [(byte 0)]
+         serialized-data (serialize-data structure)]
+     (-> (concat serialized-class serialized-headers
+                 end-headers serialized-data)
+         flatten
+         vec))))
+
+(defmethod serialize :custom-type
+  [value]
+  value)
