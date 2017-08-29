be0f6c9468f326f28ae9d1d1b4b94bbf29f870a7
Manuel Uberti <manuel@7bridges.eu>
2017-08-29 15:49:38 +0200

    Add codox dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-odbp "0.1.0-SNAPSHOT"
+(defproject org.clojars.7bridges/clj-odbp "0.1.0"
   :description "A Clojure driver for OrientDB binary protocol"
   :url "http://github.com/7bridgeseu/clj-odbp"
   :license {:name "Apache License 2.0"
@@ -7,6 +7,8 @@
                  [org.clojure/clojure "1.8.0"]
                  [midje "1.8.3"]
                  [midje-junit-formatter "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-midje "3.1.3"]
-            [lein-cloverage "1.0.9"]]
-  :aliases {"coverage" ["cloverage" "--runner" ":midje"]})
+  :plugins [[lein-cloverage "1.0.9"]
+            [lein-codox "0.10.3"]
+            [lein-midje "3.1.3"]]
+  :aliases {"coverage" ["cloverage" "--runner" ":midje"]}
+  :codox {:project {:name "clj-odbp"}})
